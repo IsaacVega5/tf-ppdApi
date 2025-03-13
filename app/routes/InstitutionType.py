081c1ae6205576e9eb8f5a090926099f9474e899
IsaacVega <isaacvega361@gmail.com>
2025-03-12 21:59:19 -0300

    [fix] Fix comments of PR

--- app/routes/InstitutionType.py
+++ app/routes/InstitutionType.py
@@ -1,9 +1,9 @@
 
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.controllers import InstitutionTypeController
 from app.db import get_session
-from app.models.InstitutionType import InstitutionTypeCreate, InstitutionTypeUpdate
+from app.models.InstitutionType import InstitutionTypeCreate, InstitutionTypeUpdate, InstitutionType
 
 router = APIRouter(
   prefix="/institution-type",
@@ -11,26 +11,26 @@ router = APIRouter(
   responses={404: {"description": "Not found"}}
 )
 
-@router.get("/")
-def get_institution_type( session = Depends(get_session), request : Request = None ):
+@router.get("/", description="List all institution types", response_model=list[InstitutionType])
+def get_institution_type( session = Depends(get_session)):
   institution_types = InstitutionTypeController.get_all(session)
   return institution_types
 
-@router.get("/{id}")
-async def get_institution_type(id, session = Depends(get_session)):
+@router.get("/{id}", description="Get institution type by id", response_model=InstitutionType)
+async def get_institution_type(id : int, session = Depends(get_session)):
   institution_type = InstitutionTypeController.get_by_id(id, session)
   if not institution_type:
-    return HTTPException(status_code=404, detail="Institution type not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Institution type not found")
   return institution_type
 
-@router.post("/")
+@router.post("/", description="Create a new institution type", response_model=InstitutionType)
 async def post_institution_type(institution_type: InstitutionTypeCreate, session = Depends(get_session)):
   return InstitutionTypeController.create_institution_type(institution_type, session)
 
-@router.delete("/{id}")
-async def delete_institution_type(id, session = Depends(get_session)):
+@router.delete("/{id}", description="Delete an institution type")
+async def delete_institution_type(id: int, session = Depends(get_session)):
   return InstitutionTypeController.delete_institution_type(id, session)
 
-@router.put("/{id}")
-async def update_institution_type(id, institution_type: InstitutionTypeUpdate, session = Depends(get_session)):
+@router.put("/{id}", description="Update an institution type", response_model=InstitutionType)
+async def update_institution_type(id: int, institution_type: InstitutionTypeUpdate, session = Depends(get_session)):
   return InstitutionTypeController.update_institution_type(id, institution_type, session)
